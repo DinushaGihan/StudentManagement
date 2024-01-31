c633e0afa15580810946f89f02833410aca4e593
Dinusha Gihan <dasanayakadmdg.20@uom.lk>
2024-01-31 21:05:14 +0530

    update student route

--- BACKEND/routes/students.js
+++ BACKEND/routes/students.js
@@ -19,4 +19,32 @@ router.route("/add").post((req,res)=>{
     })
 })
 
+router.route("/").get((req,res)=>{
+
+    Student.find().then((students)=>{
+        res.json(students)
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+
+router.route("/update/:id").put(async(req,res)=>{
+
+    let userId=req.params,id;
+    const {name,age,gender}=req.body;
+
+    const updateStudent={
+        name,
+        age,
+        gender
+    }
+
+    const update=await Student.findByIdAndUpdate(userId,updateStudent)
+    .then(()=>{
+        res.status(200).send({status:"User Updated", user:update})
+    }).catch((err)=>{
+        console.log(err);
+        res.status(500).send({status:"Error with updating data"});    
+    })
+})
 module.exports=router;
